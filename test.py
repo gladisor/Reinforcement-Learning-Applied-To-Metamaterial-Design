bb5014f0c4cd2ad1fdd27c7c885e04be850eb680
gladisor <123.tristan.shah@gmail.com>
2021-01-05 13:52:38 -0500

    stashing radii experement

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
 from tscsRL.environments.TSCSEnv import ContinuousTSCSEnv, DiscreteTSCSEnv
 from tscsRL.environments.GradientTSCSEnv import ContinuousGradientTSCSEnv, DiscreteGradientTSCSEnv
+from tscsRL.environments.RadiiTSCSEnv import ContinuousRadiiTSCSEnv, DiscreteRadiiTSCSEnv
 from tscsRL.agents import ddpg, ddqn
 from tscsRL import utils
 import imageio
 import torch
 
 ## Name of the run we want to evaluate
-name = 'ddqn4cyl0.45-0.35-8000decay'
+name = 'ddqnRadii-test'
 
 path = 'results/' + name
 env_params = utils.jsonToDict(path + '/env_params.json')
 agent_params = utils.jsonToDict(path + '/agent_params.json')
 
 ## Change this environment to whatever one you need
-env = DiscreteTSCSEnv(
-	nCyl=env_params['nCyl'],
+env = DiscreteRadiiTSCSEnv(
 	kMax=env_params['kMax'],
 	kMin=env_params['kMin'],
-	nFreq=env_params['nFreq'],
-	stepSize=env_params['stepSize'])
+	nFreq=env_params['nFreq'])
 
 ## Make sure these parameters are set from the env_params
 env.ep_len = env_params['ep_len']
@@ -36,7 +35,7 @@ agent.epsilon = 0.05
 # agent.noise_scale = 0.02
 
 ## Load weights, specify checkpoint number
-agent.load_checkpoint(path + '/checkpoints/', 8000)
+agent.load_checkpoint(path + '/checkpoints/', 3400)
 
 ## For creating a video of the episode
 writer = imageio.get_writer(name + '.mp4', format='mp4', mode='I', fps=15)
@@ -45,13 +44,13 @@ writer = imageio.get_writer(name + '.mp4', format='mp4', mode='I', fps=15)
 ## THIS WHOLE BLOCK IS THE INTERACTION LOOP
 
 ## Starting from a random config
-# state = env.reset()
+state = env.reset()
 ## End starting from random config
 
 ## Starting from a predefined config
-env.config = torch.tensor([[-4.9074,  3.9546,  2.6997,  0.7667,  0.6999,  4.5946,  4.9415, -0.2377]])
-env.counter = torch.tensor([[0.0]])
-env.setMetric(env.config)
+# env.config = torch.tensor([[-4.9074,  3.9546,  2.6997,  0.7667,  0.6999,  4.5946,  4.9415, -0.2377]])
+# env.counter = torch.tensor([[0.0]])
+# env.setMetric(env.config)
 
 env.info['initial'] = env.RMS.item()
 env.info['lowest'] = env.info['initial']
@@ -63,16 +62,16 @@ state = env.getState()
 done = False
 
 results = {
-		'config': [],
+		'radii': [],
 		'rms': [],
 		'tscs': []}
 
 while not done:
-	results['config'].append(env.config)
+	results['radii'].append(env.radii)
 	results['rms'].append(env.RMS)
 	results['tscs'].append(env.TSCS)
 
-	img = env.getIMG(env.config)
+	img = env.getIMG(env.radii)
 	writer.append_data(img.view(env.img_dim).numpy())
 
 	action = agent.select_action(state)
@@ -82,22 +81,22 @@ while not done:
 	state = nextState
 
 ## Initial stuff
-initialConfig = results['config'][0]
+initialRadii = results['radii'][0]
 initialRMS = results['rms'][0]
 initialTSCS = results['tscs'][0]
 
 ## Optimal stuff
 minIdx = results['rms'].index(min(results['rms']))
-optimalConfig = results['config'][minIdx]
+optimalRadii = results['radii'][minIdx]
 optimalRMS = results['rms'][minIdx]
 optimalTSCS = results['tscs'][minIdx]
 
 print('RESULTS:')
-print(f'Initial config: {initialConfig}')
+print(f'Initial radii: {initialRadii}')
 print(f'Initial RMS: {initialRMS}')
 print(f'Initial TSCS: {initialTSCS}')
 print()
-print(f'Min config: {optimalConfig}')
+print(f'Min radii: {optimalRadii}')
 print(f'Min rms: {optimalRMS}')
 print(f'Min tscs: {optimalTSCS}')
 
