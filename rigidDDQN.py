fb18627de0510463f60bbccb01b12c35a2790366
DiuLaMaX <linweizhuo1996@gmail.com>
2021-01-11 20:01:57 -0800

    update

--- rigidDDQN.py
+++ rigidDDQN.py
@@ -2,6 +2,13 @@ from tscsRL.environments.TSCSEnv import DiscreteTSCSEnv
 from tscsRL.environments.GradientTSCSEnv import DiscreteGradientTSCSEnv
 from tscsRL.agents import ddqn
 
+# env = DiscreteTSCSEnv(
+# 	nCyl=4,
+# 	kMax=0.45,
+# 	kMin=0.35,
+# 	nFreq=11,
+# 	stepSize=0.5)
+
 env = DiscreteGradientTSCSEnv(
 	nCyl=4,
 	kMax=0.45,
@@ -13,10 +20,16 @@ params = ddqn.default_params()
 params['batch_size'] = 256
 params['save_every'] = 500
 params['decay_timesteps'] = 8000
-params['num_episodes'] = 9000
-params['use_wandb'] = True
+params['num_episodes'] = 8500
+params['reward'] = []
+params['lowest'] = []
+params['save'] = True
+params['plot_hpc'] = True
+params['use_wandb'] = False
 
-name = 'ddqnGradientReward4cyl'
+name = 'ddqnNoGradient4cyl_run1'
+# name = 'ddqnGradient4cy_run1'
+# name = 'ddqnGradientReward4cyl_run1'
 
 agent = ddqn.DDQNAgent(
 	env.observation_space, 
